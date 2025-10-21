c05dc971a0e8602f02786cadfd75ea50e9e3fbdb
Hidaya Mohamed <hidayamohamed002@gmail.com>
2025-10-21 08:48:09 +0300

    Complete lab

--- src/pages/Actors.jsx
+++ src/pages/Actors.jsx
@@ -1,16 +1,36 @@
 import { useEffect, useState } from "react";
+import NavBar from "../components/NavBar";
 
 function Actors() {
+  const [actors, setActors] = useState([]);
+
+  useEffect(() => {
+    fetch("/db.json")
+      .then((res) => res.json())
+      .then((data) => setActors(data.actors))
+      .catch((err) => console.error("Error fetching actors:", err));
+  }, []);
+
   return (
     <>
       <header>
-        {/* What component should go here? */}
+        <NavBar />
       </header>
       <main>
-        {/* Actor info here! */}
+        <h1>Actors Page</h1>
+        {actors.map((actor) => (
+          <article key={actor.id}>
+            <h2>{actor.name}</h2>
+            <ul>
+              {actor.movies.map((movie, i) => (
+                <li key={i}>{movie}</li>
+              ))}
+            </ul>
+          </article>
+        ))}
       </main>
     </>
   );
-};
+}
 
 export default Actors;
